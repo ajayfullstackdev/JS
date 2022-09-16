125d71472c584a788c3214af6ee04f83594f8e21
AJAY KUMAR JHA <77736437+ajayjhaDev@users.noreply.github.com>
2022-09-16 15:05:04 +0530

    Update OOPs.js

--- OOPs.js
+++ OOPs.js
@@ -393,7 +393,8 @@ console.log(tajrestaurant.getThroughWaiter());
 
 <!--------------------------------------------------------->
 
-// You need to use the concept of polymophism. You need to create a parent class 'shape' which returns the area. Create different classes circle,rectangle, sqaure and triangle which extend the parent class shape and calculate the area.
+// You need to use the concept of polymorphism. You need to create a parent class 'shape' which returns the area. 
+// Create different classes circle,rectangle, sqaure and triangle which extend the parent class shape and calculate the area.
 
 // create  a method allShapes that receives an array of shapes and returns cumulative area value of all passed shapes.
 // You may diplay in HTML or console log your outputs.
